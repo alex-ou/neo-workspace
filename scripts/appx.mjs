4ef39f5adef5358b7a70470eb80629d41f444538
Alex Ou <alex.ou@live.com>
2022-08-03 18:39:02 +1000

    Added assets

--- scripts/appx.mjs
+++ scripts/appx.mjs
@@ -2,13 +2,13 @@ import convertToWindowsStore from "electron-windows-store";
 
 convertToWindowsStore({
   inputDirectory:
-    "C:\\Users\\alexo\\w\\unity-workspace\\out\\UnityWorkspace-win32-x64",
-  outputDirectory: "C:\\Users\\alexo\\w\\unity-workspace\\out\\windows-store",
+    "C:\\Users\\alexo\\w\\neo-workspace\\out\\NeoWorkspace-win32-x64",
+  outputDirectory: ".\\out\\windows-store",
+  assets: ".\\build\\appx\\",
   packageVersion: "1.0.0.0",
   packageName: "NeoWorkspace",
   packageDisplayName: "Neo Workspace",
   publisherDisplayName: "NeoNav",
   publisher: "CN=97AE30B3-5C2B-418F-AC5F-1F1B28631FF9",
-  name: "29801UnityOne.UnityWorkspace",
-  identityName: "29801UnityOne.UnityWorkspace",
+  identityName: "29801UnityOne.NeoWorkspace",
 });
